b55b2d67647b8acbded1960dd24f6930d93f801e
Serhat Yildiz <yildizserhat@yahoo.com>
2023-03-22 06:04:43 -0500

    Add main.js

---
+++ c14/simple-functions/js/main.js
@@ -0,0 +1,31 @@
+//create a function that adds two numbers and alerts the sum
+
+function addTwoNumbers(num1, num2) {
+    alert (num1 + num2)
+}
+
+addTwoNumbers (2,3)
+
+// //function addTwoNumbers (num1, num2){
+//     let sum = num1 + num2
+//     alert (sum)
+// }
+
+//create a function that multiplys three numbers and console logs the product
+
+function multiplysThreeNumbers(num1, num2, num3) {
+
+    const product = num1 * num2 * num3
+
+    console.log(product)
+}
+
+multiplysThreeNumbers(1, 2, 3)
+
+//create a function that divides two numbers and returns the ???
+
+function dividesTwoNumbers( n1, n2) {
+    return n1 / n2
+}
+
+console.log(dividesTwoNumbers(12,6))
