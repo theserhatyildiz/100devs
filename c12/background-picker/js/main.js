177fbf966a961b08a879b9430639676f694a5f00
Serhat Yildiz <yildizserhat@yahoo.com>
2023-08-01 17:41:15 +0300

    Add main.js

--- c12/background-picker/js/main.js
+++ c12/background-picker/js/main.js
@@ -1,6 +1,7 @@
-document.getElementById('purple').onclick = partyPurple
+/*document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('red').onclick = partyRed
 
 
 function partyPurple() {
@@ -16,4 +17,38 @@ function partyGreen() {
 function partyBlue() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
+}
+
+function partyRed() {
+  document.querySelector('body').style.backgroundColor = 'rgba(251,0,6,1)'
+  document.querySelector('body').style.color = 'white'
+}
+---------------------------------------*/
+
+document.getElementById('purple').onclick= partyPurple
+
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color= 'white'
+}
+
+document.getElementById('green').onclick = partyGreen
+
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+document.getElementById('blue').onclick= partyBlue
+
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.color = 'white'
+}
+
+document.getElementById('red').onclick = partyRed
+
+function partyRed() {
+  document.querySelector('body').style.backgroundColor = 'rgba(251,0,6,1)'
+  document.querySelector('body').style.color= 'white'
 }
