4bcd4226b5bd31dd64dc9b8ef4443daaec1d739e
Serhat Yildiz <yildizserhat@yahoo.com>
2023-08-04 19:15:26 +0300

    add js.main

--- c13/bachelor-code/2/js/main.js
+++ c13/bachelor-code/2/js/main.js
@@ -1,25 +1,96 @@
-const andi = document.querySelector('#andi')
+// // const andi = document.querySelector('#andi')
+
+// const andi = document.querySelector('#andi')
+
+// // const claire = document.querySelector('#claire')
+
+// const claire = document.querySelector('#claire')
+
+// // const sharleen = document.querySelector('#sharleen')
+
+// const sharleen = document.querySelector('#sharleen')
+
+// // document.querySelector('#andiNext').addEventListener('click', andiNext)
+
+// document.querySelector('#andiNext').addEventListener('click', andiNext)
+
+// // document.querySelector('#claireNext').addEventListener('click', claireNext)
+
+// document.querySelector('#claireNext').addEventListener('click', claireNext)
+
+// // document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+
+// document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+
+// // function andiNext(){
+// // 	claire.classList.add('hidden')
+// // 	sharleen.classList.add('hidden')
+// // 	andi.classList.toggle('hidden')
+// // }
+
+// function andiNext() {
+// 	claire.classList.add('hidden')
+// 	sharleen.classList.add('hidden')
+// 	andi.classList.toggle('hidden')
+// }
+
+// // function claireNext(){
+// // 	andi.classList.add('hidden')
+// // 	sharleen.classList.add('hidden')
+// // 	claire.classList.toggle('hidden')
+// // }
+
+// function claireNext()  {
+// 	andi.classList.add('hidden')
+// 	sharleen.classList.add('hidden')
+// 	claire.classList.toggle('hidden')
+// }
+
+// function sharleenNext(){
+// 	claire.classList.add('hidden')
+// 	andi.classList.add('hidden')
+// 	sharleen.classList.toggle('hidden')
+// }
+
+// function sharleenNext()  {
+// 	andi.classList.add('hidden')
+// 	claire.classList.add('hidden')
+// 	sharleen.classList.toggle('hidden')
+// }
+
 const claire = document.querySelector('#claire')
+const andi = document.querySelector('#andi')
 const sharleen = document.querySelector('#sharleen')
 
-document.querySelector('#andiNext').addEventListener('click', andiNext)
 document.querySelector('#claireNext').addEventListener('click', claireNext)
-document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
 
-function andiNext(){
-	claire.classList.add('hidden')
-	sharleen.classList.add('hidden')
-	andi.classList.toggle('hidden')
-}
+function claireNext() {
 
-function claireNext(){
 	andi.classList.add('hidden')
 	sharleen.classList.add('hidden')
 	claire.classList.toggle('hidden')
+
+
 }
 
-function sharleenNext(){
+document.querySelector('#andiNext').addEventListener('click', andiNext)
+
+function andiNext() {
+
+	andi.classList.toggle('hidden')
+	sharleen.classList.add('hidden')
 	claire.classList.add('hidden')
+
+
+}
+
+document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+
+function sharleenNext() {
+
 	andi.classList.add('hidden')
 	sharleen.classList.toggle('hidden')
+	claire.classList.add('hidden')
+
+
 }
