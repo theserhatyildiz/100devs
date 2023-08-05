711c22867f559d2fcb56a829e79d68be052b1c80
Serhat Yildiz <yildizserhat@yahoo.com>
2023-08-05 11:00:49 +0300

    Add main.js

---
+++ c14.2/simple-functions/js/main.js
@@ -0,0 +1,28 @@
+//create a function that adds two numbers and alerts the sum
+
+function addTwoNum (num1, num2){
+
+   alert(num1 + num2) 
+}
+
+addTwoNum(1,3)
+
+//create a function that multiplys three numbers and console logs the product
+
+function multiplyThreeNum(num1, num2, num3){
+
+    alert(num1 * num2 * num3)
+}
+
+multiplyThreeNum(1,2,3)
+
+//create a function that divides two numbers and returns the ???
+
+function divideTwoNum (num1, num2) {
+
+
+    return num1/num2
+
+}
+
+divideTwoNum ( 4, 2)
