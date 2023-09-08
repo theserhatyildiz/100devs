fcebd085cf762d8b7d34f1bef2d1efecc61c1cb1
Serhat Yildiz <yildizserhat@yahoo.com>
2023-09-08 14:06:07 +0300

    Add main.js

---
+++ c22/objects-lost-galaxy/js/main.js
@@ -0,0 +1,60 @@
+//Create a mouse object that has four properties and three methods
+
+let mouse = {}
+
+mouse.brand = 'Logitech'
+mouse.color = 'black'
+mouse.mode = 'MX Ergo'
+mouse.wireless = true
+
+mouse.leftClick = function () {
+    console.log('left click')
+}
+
+mouse.rightClick = function () {
+    console.log('right click')
+}
+
+mouse.scroll = function () {
+    console.log('jump jump jump')
+}
+
+
+let mouse2 = {}
+
+mouse2.brand = 'Logitech'
+mouse2.color = 'black'
+mouse2.mode = 'Pro Wireless'
+mouse2.wireless = true
+
+mouse2.leftClick = function () {
+    console.log('left click')
+}
+
+mouse2.rightClick = function () {
+    console.log('right click')
+}
+
+mouse2.scroll = function () {
+    console.log('jump jump jump')
+}
+
+let mouse3 = {}
+
+mouse3.brand = 'Apple'
+mouse3.color = 'silver'
+mouse3.mode = 'Magic Trackpad 2'
+mouse3.wireless = true
+
+mouse3.leftClick = function () {
+    console.log('left click')
+}
+
+mouse3.rightClick = function () {
+    console.log('right click')
+}
+
+mouse3.scroll = function () {
+    console.log('slide two fingers')
+}
+
