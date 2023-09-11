b57e78170140eb69ec487d773f4320852e67b227
Serhat Yildiz <yildizserhat@yahoo.com>
2023-09-11 14:49:18 +0300

    Add main.js

---
+++ c23/objects-lost-galaxy/js/main.js
@@ -0,0 +1,21 @@
+//Create a dog object that has four properties and three methods
+
+let dog = {}
+
+dog.breed = 'poodle'
+dog.color = 'golden'
+dog.legs = 4
+dog.name = 'Giovanni'
+
+dog.bark = function() {
+    console.log('Woof')
+}
+dog.jump = function () {
+    console.log('Blast off')
+}
+dog.fetch = function() {
+    console.log('zooooom')
+}
+
+
+
