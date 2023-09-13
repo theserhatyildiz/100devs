aa7d5e8192c059f968865634e9af5b7b30e3a663
Serhat Yildiz <yildizserhat@yahoo.com>
2023-09-13 14:25:23 +0300

    All of the files

---
+++ c26/objects-lost-galaxy/js/main.js
@@ -0,0 +1,20 @@
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza. size = 'large'
+pizza. toppings = ['spinach', 'onion', 'jalapenos', 'banana peppers', 'garlic']
+pizza.crust = 'stuffed'
+pizza. sauce = 'heavy'
+
+pizza.estimatedDeliveryTime = function (){
+console.log('Calculating...')
+}
+
+pizza.burnMouth = function () {
+console.log('FFJkadjfl;kasjdfifi')
+}
+
+pizza.frisbee = function() { 
+console.log('YEEEeeeetttt')
+}
