cde8530eb1a01353948963948421a507190eba9b
Serhat Yildiz <yildizserhat@yahoo.com>
2023-09-14 17:18:13 +0300

    All

---
+++ c27/api-template/js/main.js
@@ -0,0 +1,21 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=hgEIKSzD14QAQufyGEJfkqxm3a5Vic8n4FOzyhCW&date=${choice}`
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
