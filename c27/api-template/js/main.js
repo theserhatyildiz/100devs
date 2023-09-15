c9f25523d66a505218fc25333ed81e1e963e7294
Serhat Yildiz <yildizserhat@yahoo.com>
2023-09-15 13:29:10 +0300

    all

--- c27/api-template/js/main.js
+++ c27/api-template/js/main.js
@@ -11,8 +11,18 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('img').src = data.hdurl
+        if(data.media_type === 'image') {
+
+          document.querySelector('img').src = data.hdurl
+
+        } else if(data.media_type === 'video') {
+
+          document.querySelector('iframe').src = data.url
+
+        }
+        
         document.querySelector('h3').innerText = data.explanation
+        
       })
       .catch(err => {
           console.log(`error ${err}`)
